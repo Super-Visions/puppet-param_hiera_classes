7ff3d4ad8240fe4df9a466ab6c75b03bb07b7f3b
Stefan Goethals <stefan@zipkid.eu>
2013-04-09 10:44:25 +0200

    Correct naming

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 #
 # Prametrised classes called by Hiera
 #
-class p_h_classes ( $p_h_classes = hiera_hash('p_h_classes', {} ) )
+class param_hiera_classes ( $p_h_classes = hiera_hash('p_h_classes', {} ) )
 {
   validate_hash( $p_h_classes )
   if( ! empty( $p_h_classes ) )
